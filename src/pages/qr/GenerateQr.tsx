3dc4ddc16fcfa4b88d58080ef13e0c7121a0fbc7
VigneshFenrir <151980728+VigneshFenrir@users.noreply.github.com>
2025-05-27 14:14:22 +0530

    Update GenerateQr.tsx
    Type changed

--- src/pages/qr/GenerateQr.tsx
+++ src/pages/qr/GenerateQr.tsx
@@ -9,7 +9,7 @@ import { toast } from "sonner";
 export type ProductType = {
   productName: string;
   color: string;
-  price: number;
+  price: number|string;
 };
 
 const GenerateQR: React.FC = () => {
@@ -17,7 +17,7 @@ const GenerateQR: React.FC = () => {
   const [productData, setProductData] = useState<ProductType>({
     productName: "",
     color: "",
-    price: 0,
+    price: "", 
   });
   const [error, setError] = useState<string>("");
   const [qrValue, setQrvalue] = useState<string>("");
