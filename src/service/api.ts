7c3f9a3ab423e16cf93f74935a22313f0fff7dbc
maatson <exim@maatson.com>
2025-05-27 14:01:09 +0530

    Update api.ts
    backend adjusted

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios"
 // https://my-qr-server.onrender.com/api
 const api = axios.create({
-    baseURL:"http://localhost:8000/api",
+    baseURL:"https://my-qr-server.onrender.com/api",
      headers: {
     'Content-Type': 'application/json',
   },
